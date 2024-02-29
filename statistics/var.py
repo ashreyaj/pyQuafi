32b3c4064c83f8bef41a5fce17e1d4eca203be21
Ashreya Jayaram <ashreyajram@gmail.com>
2024-02-29 19:35:07 +0530

    VaR using Monte Carlo

--- statistics/var.py
+++ statistics/var.py
@@ -34,7 +34,25 @@ class VaR:
         for s in self.stocks:
             mu = data[f'{s} Returns'].mean()
             sigma = data[f'{s} Returns'].std()
-            var_data[s] = investment[s] * (mu * self.nDays - np.sqrt(self.nDays) * sigma * norm.ppf(1-self.confidence))
+            var_data[s] = self.investment[s] * (mu * self.nDays - np.sqrt(self.nDays) * sigma * norm.ppf(1-self.confidence))
+        return var_data
+    
+class Var_MC(VaR):
+    def __init__(self, stocks, investment, confidence, nDays, start, end, iterations):
+        super().__init__(stocks, investment, confidence, nDays, start, end)
+        self.iterations = iterations
+
+    def var(self):
+        data = self.returns()
+        var_data = {}
+        for s in self.stocks:
+            mu = data[f'{s} Returns'].mean()
+            sigma = data[f'{s} Returns'].std()
+            rand = np.random.normal(0, 1, [1,self.iterations])
+            S = investment[s] * np.exp(self.nDays * (mu - 0.5 * sigma**2) + sigma * np.sqrt(self.nDays) * rand)
+            S = np.sort(S)
+            percentile = np.percentile(S, (1 - self.confidence) * 100)
+            var_data[s] = np.mean(S - percentile)
         return var_data
     
 if __name__ == '__main__':
@@ -46,3 +64,9 @@ if __name__ == '__main__':
     nDays = 1
     var = VaR(stocks, investment, confidence, nDays, start, end)
     v = var.var_historical()
+    print(f"Value at risk (Historical method): {v}")
+
+    iterations = 10000
+    var_mc = Var_MC(stocks, investment, confidence, nDays, start, end, iterations)
+    v = var_mc.var()
+    print(f"Value at risk (Monte Carlo): {v}")
