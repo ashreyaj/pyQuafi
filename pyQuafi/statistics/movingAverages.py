cec579ecbf5c83dc97faa4273f9034542a3560a1
Ashreya Jayaram <ashreyajram@gmail.com>
2024-03-08 01:51:56 +0530

    Add exponential moving average

--- pyQuafi/statistics/movingAverages.py
+++ pyQuafi/statistics/movingAverages.py
@@ -20,11 +20,20 @@ class MovingAverage:
         sma = data.rolling(window=self.period).mean()
         return sma
     
+    def ema(self):
+        data = self.get_data()
+        ema = data.ewm(span=self.period, adjust=False).mean()
+        return ema
+    
 if __name__=='__main__':
     ma = MovingAverage('AAPL', datetime.datetime(2017,1,1), datetime.datetime(2023,1,1), 200)
+    stock_price = ma.get_data()
+    stock_price.plot(label='Stock price')
     sma = ma.sma()
-    sma.plot()
+    sma.plot(label='SMA')
+    ema = ma.ema()
+    ema.plot(label='EMA')
     plt.xlabel('Date')
-    plt.ylabel('Stock price')
+    plt.legend()
     plt.show()
     
